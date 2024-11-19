fa0876690c654944fb8bf2bd3c06ea8cf4bebe43
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-11-19 13:24:15 +0200

    Init repo

---
+++ SimpleWebApp/Program.cs
@@ -0,0 +1,62 @@
+using Azure.Identity;
+
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Azure;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+namespace SimpleWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddInMemoryTokenCaches();
+
+            builder.Services.AddAuthorization(options =>
+            {
+                // By default, all incoming requests will be authorized according to the default policy.
+                options.FallbackPolicy = options.DefaultPolicy;
+            });
+
+            builder.Services.AddControllersWithViews();
+
+            // Add services to the container.
+            builder.Services.AddRazorPages()
+                .AddMicrosoftIdentityUI();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapControllers();
+
+            app.MapGet("/debug-view", context => {
+                var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+                return context.Response.WriteAsync(((IConfigurationRoot)configuration).GetDebugView());
+            });
+
+            app.Run();
+        }
+    }
+}
