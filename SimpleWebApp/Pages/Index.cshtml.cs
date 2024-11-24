26adedd8308680728f4d56b1ea558bdf4fa44360
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-11-24 10:04:28 +0200

    - Modify code

--- SimpleWebApp/Pages/Index.cshtml.cs
+++ SimpleWebApp/Pages/Index.cshtml.cs
@@ -1,15 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleWebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public readonly string ImageVersion = string.Empty;
+        public readonly string Environment = string.Empty;
+
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
         {
+            this._configuration = configuration;
             _logger = logger;
+            ImageVersion = this._configuration.GetValue<string>("GIT_HASH") ?? string.Empty;
+            Environment = this._configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
         }
 
         public void OnGet()
