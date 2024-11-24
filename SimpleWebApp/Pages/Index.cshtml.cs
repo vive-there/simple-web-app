a639fe672bba50a7872c6ca5fbe3cffce733edd0
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-11-24 11:35:26 +0200

    - Modify code

--- SimpleWebApp/Pages/Index.cshtml.cs
+++ SimpleWebApp/Pages/Index.cshtml.cs
@@ -11,13 +11,15 @@ namespace SimpleWebApp.Pages
 
         public readonly string ImageVersion = string.Empty;
         public readonly string Environment = string.Empty;
+        public readonly string HostName = string.Empty;
 
         public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
         {
             this._configuration = configuration;
             _logger = logger;
             ImageVersion = this._configuration.GetValue<string>("GIT_HASH") ?? string.Empty;
-            Environment = this._configuration.GetValue<string>("WEBSITE_HOSTNAME") ?? string.Empty;
+            Environment = this._configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
+            HostName = this._configuration.GetValue<string>("WEBSITE_HOSTNAME") ?? string.Empty;
         }
 
         public void OnGet()
@@ -26,3 +28,4 @@ namespace SimpleWebApp.Pages
         }
     }
 }
+
